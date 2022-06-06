4f46681e53560e273dfbd632babe6b59c006b51a
bocong <106733667+bo-cong@users.noreply.github.com>
2022-06-06 21:45:55 +0800

    Add files via upload

---
+++ mlapp/mlapp/about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+def show_about_page():
+    st.title('about')
+    st.write('''Input 3 topics of interest and see which would be the most optimal for you to study.(can select subjects,grades and time last studied but predictor doesnt work)''')
+    st.write('''comes with a timer to time yourself for study sessions (works)''')
+    st.write('''ideated and coded entirely by bo cong''')
---
+++ mlapp/mlapp/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_timer_page
+from about import show_about_page
+page = st.sidebar.selectbox("Tab",('About','Timer','Which topic should I study?',))
+if page=='Timer':
+    show_timer_page()
+elif page=='Which topic should I study?':
+    show_predict_page()
+else:
+    show_about_page()
